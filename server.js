6b13f7278d5e11cf9b51dd956cfcb48337f52267
Timothy Dao <timster78811@gmail.com>
2018-07-30 17:04:56 -0400

    finsihed

--- server.js
+++ server.js
@@ -13,16 +13,28 @@ const express = require('express');
 const app = express();
 
 // ADD STATIC SERVER HERE
-app.use(express.static('public'))
+app.use(express.static('public'));
 
-app.listen(8080, function () {
+app.listen(8000, function () {
   console.info(`Server listening on ${this.address().port}`);
 }).on('error', err => {
   console.error(err);
 });
+
 app.get('/api/notes', (req, res) => {
     res.json(data);
-  });
-  app.get('/api/notes:id', (req, res) => {
+    const searchTerm = req.query.searchTerm;
+    if(searchTerm){
+        let filteredList = data.filter(function(item){
+            return item.title.includes(searchTerm);
+        });
+        res.json(filteredList);
+    } else{
+        res.json(data);
+    }
     res.json(data);
+  });
+  app.get('/api/notes/:id', (req, res) => {
+    const id = req.params.id;
+    res.json(data.find(item => item.id === Number(id)));
   });
